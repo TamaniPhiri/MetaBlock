4d8ee9a004bbd895e3414fad9130bb851385ac7d
tamani phiri <tamanigabriel0@gmail.com>
2024-03-19 15:14:53 +0200

    c

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,49 @@
 import { View, Text } from "react-native";
+import PagerView from "react-native-pager-view";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <PagerView
+        style={{ width: "100%", height: 200 }}
+        overScrollMode={"always"}
+        overdrag
+        initialPage={0}
+      >
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(255, 253, 175, 0.8)",
+          }}
+          key="1"
+        >
+          <Text style={{ color: "white" }}>First page</Text>
+          <Text>Swipe ➡️</Text>
+        </View>
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "yellow",
+          }}
+          key="2"
+        >
+          <Text style={{ color: "white" }}>Second page</Text>
+        </View>
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "white",
+          }}
+          key="3"
+        >
+          <Text style={{ color: "white" }}>Third page</Text>
+        </View>
+      </PagerView>
+    </SafeAreaView>
   );
 };
 
